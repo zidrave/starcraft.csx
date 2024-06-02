f70ec2c58620e1df57849358a0d36cad5558b5d8
zidrave <92062208+zidrave@users.noreply.github.com>
2024-06-01 20:07:24 -0500

    Add files via upload

---
+++ oldmaps/#/1/[ 4vs4 Zero Clutter ].scm
@@ -0,0 +1,119 @@
+<!--
+DOCUMENT_ROOT = /home/catch/htdocs
+GATEWAY_INTERFACE = CGI-Perl/1.1
+HTTP_ACCEPT = */*
+HTTP_CACHE_CONTROL = max-age=259200
+HTTP_CONNECTION = keep-alive
+HTTP_HOST = tifanie.owns1.com
+HTTP_REFERER = http://tifanie.owns1.com/Maps/download/
+HTTP_USER_AGENT = Mozilla/4.0 (compatible; MSIE; Win32)
+HTTP_VIA = 1.1 squid1.0catch.com:80 (squid/2.5.STABLE2-20030502)
+HTTP_X_FORWARDED_FOR = 66.81.116.121
+MOD_PERL = mod_perl/1.26
+PATH = /sbin:/usr/sbin:/bin:/usr/bin:/usr/X11R6/bin
+PERL_SEND_HEADER = On
+QUERY_STRING = 
+REMOTE_ADDR = 209.63.57.76
+REMOTE_PORT = 16331
+REQUEST_METHOD = GET
+REQUEST_URI = /Maps/download/%5b%204vs4%20Zero%20Clutter%20%5d.scm
+SCRIPT_FILENAME = /home/catch/cgi-bin/parse.pl
+SCRIPT_NAME = /Maps/download/[ 4vs4 Zero Clutter ].scm
+SCRIPT_URI = http://www13.0catch.com/Maps/download/[ 4vs4 Zero Clutter ].scm
+SCRIPT_URL = /Maps/download/[ 4vs4 Zero Clutter ].scm
+SERVER_ADDR = 192.168.100.13
+SERVER_ADMIN = root@www2.0catch.com
+SERVER_NAME = www13.0catch.com
+SERVER_PORT = 80
+SERVER_PROTOCOL = HTTP/1.0
+SERVER_SIGNATURE = <ADDRESS>Apache/1.3.20 Server at www13.0catch.com Port 80</ADDRESS>
+
+SERVER_SOFTWARE = Apache/1.3.20 (Unix) mod_gzip/1.3.19.1a mod_perl/1.26
+UNIQUE_ID = QJNGFX8AAAEAAHvbDh0
+-->
+<html>
+<head>
+<title>FILE NOT FOUND! - web hosting, ecommerce web hosting, web site hosting, web hosting services, web hosting
+provider, internet hosting, cheap web hosting</title>
+
+<META NAME="KEYWORDS" CONTENT="web hosting, ecommerce web hosting, web site hosting, web hosting services, web hosting
+provider, internet hosting, cheap web hosting, web site hosting, web hosting services, web hosting provider, internet hosting,
+cheap web hosting, web hosting, ecommerce web hosting web hosting services web hosting provider internet hosting cheap web
+hosting web hosting, ecommerce web hosting web site hosting web hosting provider internet hosting cheap web hosting web
+hosting, ecommerce web hosting web site hosting web hosting services internet hosting, cheap web hosting, web hosting,
+ecommerce web hosting, web site hosting, web hosting services, web hosting provider, cheap web hosting, web hosting, ecommerce
+web hosting, web site hosting, web hosting services web hosting provider, internet hosting"> 
+
+<base href="http://www.0catch.com/">
+<style type="text/css">
+<!--
+.link {font:10px verdana; text-decoration:none; font-weight:bold; color:#003366; }
+.foot { font:10px verdana; text-decoration:none; font-weight:bold; color:#ffb800; }
+A:hover { color:#349901; }
+-->
+</style>
+</head>
+<body bgcolor="#003366" text="#000000" link="GREEN" alink="GREEN" vlink="GREEN">
+<div align="center"><table cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td><img src="http://images.1freewebspace.com/partner_greenonblue.gif" width="178" height="150" border="0" alt="web hosting, ecommerce web hosting, web
+site hosting, web hosting services, web hosting provider, internet hosting, cheap web hosting, web site hosting, web hosting
+services, web hosting provider"></td>
+		<td><table cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td><img src="images/b2top1.gif" width="495" height="31" border="0"><br>
+	<a href="http://tifanie.owns1.com/tj_bc"><img src="http://tifanie.owns1.com/tj_bs" width="468" height="60" border="0"></a>
+<img src="http://banner.0catch.com/cgi-bin/error_count.gif" height="1" and width="1">
+    </td>
+  </tr>
+	<tr>
+		<td background="images/b2top2.gif" width="495" height="59" valign="top"><nobr><img
+src="images/clear.gif" width="60" height="40" border="0"><font face="verdana" size="1"><b><a
+href="http://www.owns1.com/" class="link">
+LOGIN</a> &nbsp; <a href="http://info.owns1.com/contactus" class="link">CONTACT US</a>
+&nbsp; <a
+href="http://info.owns1.com/techsupport" class="link">TECH SUPPORT</a> &nbsp; <a
+href="http://info.owns1.com/upgrade" class="link">UPGRADE YOUR SITE</a></b></font></nobr></td>
+	</tr>
+</table></td>
+  </tr>
+</table>
+<table cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td background="images/b2left1.gif"><img src="images/clear.gif" width="49" height="1" border="0"></td>
+		<td width="577" bgcolor="#ffffff" valign="top"><font face="arial,helvetica" size="4" color="#003366">
+<b>File Not Found!</b></font><br><br>
+<font face="arial,helvetica" size="2"><br>
+
+The file you are searching for is not located on our servers.  Please check your spelling.  File names may be case sensitive.  Please verify the URL as this is the most common reason for this error.<br>
+
+<br><br><b>If you would like your own 100 MB FREE WEB SITE with a long list of features simply sign up at <a href="http://www.owns1.com">owns1.com.</a></b><!/font></td>
+		<td background="images/b2right.gif"><img src="images/clear.gif" width="47" height="1" border="0"></td>
+  </tr>
+	<tr><td colspan="3"><img src="images/b2bot.gif" width="673" height="43" border="0"></td></tr>
+	<tr><td colspan="3"><img src="images/b2bot2.gif" width="212" height="16" border="0"><nobr><img
+src="images/clear.gif" width="12" height="1" border="0"><font face="verdana" size="1"><b><a
+href="http://www.owns1.com/" class="foot">LOGIN</a> &nbsp; <a
+href="http://info.owns1.com/contactus" class="foot">CONTACT US</a> &nbsp; <a
+href="http://info.owns1.com/techsupport" class="foot">TECH SUPPORT
+</a> &nbsp; <a href="http://info.owns1.com/upgrade" class="foot">UPGRADE YOUR SITE</a></b></font></nobr></td></tr>
+	<tr><td colspan="3" align="center"><br>
+<table border=0 valign=top align=left width=123><td align=right><a
+	href="http://www.thefreesite.com/" target="_top"><img src="http://www.0catch.com/freesitee.gif" border=0></a><br> <a
+	href="http://www.ez-freebies.com/" target="_top"><img src="http://www.0catch.com/ezfree.gif" border=0></a></td></table> 
+<a href="http://tifanie.owns1.com/tj_bc"><img src="http://tifanie.owns1.com/tj_bs" width="468" height="60" border="0"></a>
+
+<!192.168.100.13 >
+
+<!-- AUTO_PROMPT AD START -->
+<!script language="JavaScript" type="text/JavaScript" src="http://banner.0catch.com/cgi-bin/netpal.js"><!/script>
+<!-- AUTO_PROMPT AD END -->
+
+<script language="JavaScript" src="http://webpdp.gator.com/4/placement/431/"></script>
+<script language="javascript" src="http://banner.0catch.com/cgi-bin/popup.js"></script>
+<!IFRAME SRC="http://ads.adtomi.com/ads/servePopADP101.php?03" WIDTH=0 HEIGHT=0 BORDER=0 FRAMEBORDER=0 MARGINWIDTH=0
+MARGINHEIGHT=0><!/IFRAME><!ADTOMI>
+</tr>
+</table></div>
+</body>
+</html>
